d4d4d8c2d2df0c3d75da03ded47be0528432bd22
Franklin <franklin.op@hotmail.com>
2023-12-03 10:32:19 +0100

    adding schema tables from pg core

---
+++ src/lib/db/schema.ts
@@ -0,0 +1,8 @@
+import { pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+
+export const chats = pgTable('chats', {
+  id: serial('id').primaryKey(),
+  pdfName: text('pdf_name').notNull(),
+  pdfUrl: text('pdf_url').notNull(),
+  createdAt: timestamp('created_at').notNull(),
+});
