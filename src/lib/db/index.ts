cf1665a83b39db89d3ce2f5c3a60d9649af8a501
Franklin <franklin.op@hotmail.com>
2023-12-03 10:43:05 +0100

    creating table for saved chats

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -5,7 +5,6 @@ import { drizzle } from 'drizzle-orm/neon-http';
 if (!process.env.DATABASE_URL) {
   throw new Error('database url not found');
 }
-
 const sql = neon(process.env.DATABASE_URL);
 
 export const db = drizzle(sql);
