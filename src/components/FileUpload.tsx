987b92d31dc022cd5709323ed229a9be9891e764
Franklin <franklin.op@hotmail.com>
2023-12-04 19:54:07 +0100

    added css to file upload

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,7 +1,10 @@
+'use client';
+
 import React from 'react';
+import { useDropzone } from 'react-dropzone';
 
 const FileUpload = () => {
-  return <div>FileUpload</div>;
+  return <div className="p-2 bg-white rounded-xl"></div>;
 };
 
 export default FileUpload;
